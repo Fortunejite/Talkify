648e47d5f14afa071cc2f7d779a1d6558e06eb40
Your Name <you@example.com>
2023-06-11 20:39:10 +0100

    m

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from package import app
+from package import app, socketio
 
 if __name__ == '__main__':
     """
     Entry point of the application.
     """
-    app.run(socket=app.config['SERVER_NAME'])
+    socketio.run(app, host='127.0.0.1', port=8000)
+
