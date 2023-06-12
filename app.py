82cf0b73c44a0f4d548338748414a9cd8905d2d0
Your Name <you@example.com>
2023-06-12 06:21:54 +0000

    m

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from package import app, socketio
+from gevent import monkey
+monkey.patch_all()
 
 if __name__ == '__main__':
     """
     Entry point of the application.
     """
-    socketio.run(app, host='0.0.0.0', port=8000, threaded=True)
+    socketio.run(app, host='0.0.0.0', port=8000)
     
