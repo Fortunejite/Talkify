9fa92428487c110702fcf8f3d59c406d86b7b4f7
Your Name <you@example.com>
2023-06-21 02:39:43 +0000

    bugs fixed

--- package/static/js/chat.js
+++ package/static/js/chat.js
@@ -1,83 +1,17 @@
-function get_messages(name, code) {
-  $.ajax({
-    url: '/chat/' + name,
-    type: 'GET',
-    success: function(response) {
-      const messages = response.messages;
-      if ($(window).width() > 800) {                                                                                                                                             $('.right-container').css('opacity', 0);                                                                                                                                $('.right-container').html(code);                                                                                                                                    }
-      $('#friend').text(name)
-
-      $('.dp').attr('src', "/image/"+name);
-      $('.dp').attr('alt', name);
-
-      const parent = $('.chat-container')
-      parent.empty();
-      if (messages) {
-        for (let i = 0; i < messages.length; i++) {
-          const message = messages[i];
-          const text = message['body'];
-          const date = message['time'];
-          const owner = message['sent_by'];
-          
-          if (owner == name) {
-            const new_message = $('<div class="message-box friend-message"></div>')
-            new_message.append('<p>' + text + '<br><span>' + date + '</span></p>');
-            parent.append(new_message)
-          } else {
-            const new_message = $('<div class="message-box my-message"></div>')
-            new_message.append('<p>' + text + '<br><span>' + date + '</span></p>');
-            parent.append(new_message)
-          }
-        }
-      }
-      if ($(window).width() < 800) {
-        $('.right-container').css('display', 'block');
-        $('.left-container').css('display', 'none');
-      } else {
-        $('.right-container').css('opacity', 1);
-      }
-      $("#send").click(sendClick);
-      $('#back').click(backClick);
-    }
-  });
-}
-
-function send_message (name) {
-  const data = {
-    'body': $('#message').val(),
-    'sent_by': name
-  };
-  $.ajax ({
-    url: '/chat/' + $('#friend').text() + '/send',
-    type: 'POST',
-    data: data,
-    success: function(response) {
-      $('#message').val('')
-      const parent = $('.chat-container')
-      const new_message = $('<div class="message-box my-message"></div>')
-      new_message.append('<p>' + response.body + '<br><span>' + response.time + '</span></p>');
-      parent.append(new_message)
-      const div = $('.chat-container');
-      div.scrollTop(div[0].scrollHeight)
-
-    }
-
-  })
-}
-
-
-
 $(document).ready(function() {
-    function sendClick (event) {
+  const codes = $('.right-container').html();
+  const name = $('h6').text();
+
+  function sendClick(event) {
     event.preventDefault();
     if ($('#message').val()) {
-      send_message($('h6').text())
-    } else{
-      alert('Pls type your message');
+      send_message(name);
+    } else {
+      alert('Please type your message');
     }
-}
+  }
 
-function backClick() {
+  function backClick() {
     if ($(window).width() < 800) {
       // Execute code for small screens
       $('.right-container').css('display', 'none');
@@ -87,44 +21,114 @@ function backClick() {
       $('.right-container').empty();
       $('.right-container').append('<h2>Click on a friend to chat</h2>');
     }
-}
-  const name = $('h6').text();
-  const codes = $('.right-container').html();
+  }
+
+  function get_messages(name, code) {
+    $.ajax({
+      url: '/chat/' + name,
+      type: 'GET',
+      success: function(response) {
+        const messages = response.messages;
+        if ($(window).width() > 800) {
+          $('.right-container').css('opacity', 0);
+          $('.right-container').html(code);
+        }
+        $('#friend').text(name);
+
+        $('.dp').attr('src', "/image/" + name);
+        $('.dp').attr('alt', name);
+
+        const parent = $('.chat-container');
+        parent.empty();
+        if (messages) {
+          for (let i = 0; i < messages.length; i++) {
+            const message = messages[i];
+            const text = message['body'];
+            const date = message['time'];
+            const owner = message['sent_by'];
+
+            if (owner == name) {
+              const new_message = $('<div class="message-box friend-message"></div>');
+              new_message.append('<p>' + text + '<br><span>' + date + '</span></p>');
+              parent.append(new_message);
+            } else {
+              const new_message = $('<div class="message-box my-message"></div>');
+              new_message.append('<p>' + text + '<br><span>' + date + '</span></p>');
+              parent.append(new_message);
+            }
+          }
+        }
+        if ($(window).width() < 800) {
+          $('.right-container').css('display', 'block');
+          $('.left-container').css('display', 'none');
+        } else {
+          $('.right-container').css('opacity', 1);
+        }
+
+        // Scroll to the bottom of the container
+        parent.scrollTop(parent[0].scrollHeight);
+
+        $("#send").click(sendClick);
+        $('#back').click(backClick);
+      }
+    });
+  }
+
+  function send_message(name) {
+    const data = {
+      'body': $('#message').val(),
+      'sent_by': name
+    };
+    $.ajax({
+      url: '/chat/' + $('#friend').text() + '/send',
+      type: 'POST',
+      data: data,
+      success: function(response) {
+        $('#message').val('');
+        const parent = $('.chat-container');
+        const new_message = $('<div class="message-box my-message"></div>');
+        new_message.append('<p>' + response.body + '<br><span>' + response.time + '</span></p>');
+        parent.append(new_message);
+        const div = $('.chat-container');
+        div.scrollTop(div[0].scrollHeight);
+        parent.scrollTop(parent[0].scrollHeight);
+      }
+    });
+  }
+
   if ($(window).width() > 800) {
     $('.right-container').empty();
     $('.right-container').append('<h2>Click on a friend to chat</h2>');
   }
 
-  
-  
-  $('.chat-box').click(function (event) {
+  $('.chat-box').click(function(event) {
     if ($(window).width() < 800) {
-      get_messages($(this).find('#ffriend').text())
+      get_messages($(this).find('#ffriend').text());
       let socket = io();
       let url = $(this).find('#ffriend').text() + '-' + name;
-      socket.on(url, function(data){
+      socket.on(url, function(data) {
         if (data['sent_by'] == $('#friend').text()) {
-          const parent = $('.chat-container')
-          const new_message = $('<div class="message-box friend-message"></div>')
+          const parent = $('.chat-container');
+          const new_message = $('<div class="message-box friend-message"></div>');
           new_message.append('<p>' + data['body'] + '<br><span>' + data['time'] + '</span></p>');
-          parent.append(new_message)
+          parent.append(new_message);
           const div = $('.chat-container');
-          div.scrollTop(div[0].scrollHeight)
+          div.scrollTop(div[0].scrollHeight);
         }
       });
     } else {
       $('.right-container').empty();
-      get_messages($(this).find('#ffriend').text(), codes)
+      get_messages($(this).find('#ffriend').text(), codes);
       let socket = io();
       let url = $(this).find('#ffriend').text() + '-' + name;
-      socket.on(url, function(data){
+      socket.on(url, function(data) {
         if (data['sent_by'] == $('#friend').text()) {
-          const parent = $('.chat-container')
-          const new_message = $('<div class="message-box friend-message"></div>')
+          const parent = $('.chat-container');
+          const new_message = $('<div class="message-box friend-message"></div>');
           new_message.append('<p>' + data['body'] + '<br><span>' + data['time'] + '</span></p>');
-          parent.append(new_message)
+          parent.append(new_message);
           const div = $('.chat-container');
-          div.scrollTop(div[0].scrollHeight)
+          div.scrollTop(div[0].scrollHeight);
         }
       });
     }
@@ -142,8 +146,7 @@ function backClick() {
     window.location.href = '/notifications';
   });
 
-  $('#users').click(function(event){
+  $('#users').click(function(event) {
     window.location.href = '/users';
   });
-
 });
